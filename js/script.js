6c6b0236d229bab924365f93eeb0a623a3e96f43
Christoph Leitner <itsolutions@gmail.com>
2026-06-25 09:30:11 +0200

    Premiun Startseite erstellt und Probleme behoben

--- js/script.js
+++ js/script.js
@@ -27,6 +27,12 @@ faqItems.forEach((item) => {
   const question = item.querySelector(".faq-question");
 
   question.addEventListener("click", () => {
+    faqItems.forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove("active");
+      }
+    });
+
     item.classList.toggle("active");
   });
 });
